3c39b716d5a66316f5078965b978d21976837799
Philip Mutua <pmutua@live.com>
2019-01-03 15:21:25 +0300

    demonstrating the use of an assignment statement to change the value stord in variable

---
+++ Geometry.java
@@ -0,0 +1,20 @@
+//********************************************************************
+// Geometry.java Author: Philip/pmutua
+//
+// Demonstrates the use of an assignment statement to change the
+// value stored in a variable.
+//*******************************************************************
+public class Geometry {
+    public static void main(String[] args) {
+        int sides = 7; // declaration with initialization
+        System.out.println("A heptagon has " + sides + " sides.");
+
+        sides = 10; // assigment statement
+
+        System.out.println("A decagon has " + sides + " sides.");
+
+        sides = 12;
+        System.out.println("A dodedecagon has " + sides + " sides.");
+
+    }
+}
