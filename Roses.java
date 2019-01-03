52164b831152ed7a5dbbe90bbb0137df4afdb745
Philip Mutua <pmutua@live.com>
2019-01-03 15:03:58 +0300

    completed: self-review questions next to variables and assignment

--- Roses.java
+++ Roses.java
@@ -8,8 +8,11 @@ public class Roses {
     // Prints a poem (of sorts) on multiple lines.
     // -----------------------------------------------------------------
     public static void main(String[] args) {
-        System.out.println(
-                "Roses are red,\n\tViolets are blue,\n" + "Sugar is sweet,\n\tBut I have \"commitment issues\",\n\t"
-                        + "So I'd rather just be friends\n\tAt this point in our " + "relationship.");
+        System.out.println("Roses are red,\n\tViolets are blue,\n" + "Sugar is sweet,\n\tBut I have\"commitment issues\",\n\t"
+        + "So I'd rather just be friends\n\tAt this point in our " +
+        "relationship.");
+        System.out.println("\"I made this letter longer than usual because I lack the time to"
+                + "\nmake it short.\"\n\tBlaise Pascal");
+
     }
 }
