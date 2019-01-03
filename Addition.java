7ff338934799432efbb0de0dd77830d0fa2bd517
Philip Mutua <pmutua@live.com>
2019-01-03 14:46:36 +0300

    demonstrating difference between the addition and string concatenation

---
+++ Addition.java
@@ -0,0 +1,12 @@
+//********************************************************************
+// Addition.java   Author: Philip/pmutua
+//
+// Demonstrates the difference between the addition and string
+// concatenation operators.
+//********************************************************************
+public class Addition {
+    public static void main(String[] args) {
+        System.out.println("24 and 34 concatenated: " + 24 + 34);
+        System.out.println("24 and 34 added: " + (24 + 34));
+    }
+}
