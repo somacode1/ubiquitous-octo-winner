0865c491629d136c15a1ad89ccf5983239de0b6a
Philip Mutua <pmutua@live.com>
2019-01-03 15:14:44 +0300

    demostrating the declaration,initialization and use of integer variable

---
+++ PianoKeys.java
@@ -0,0 +1,19 @@
+//********************************************************************
+// PianoKeys.java Author: Philip/pmutua
+//
+// Demonstrates the declaration, initialization, and use of an
+// integer variable.
+//********************************************************************
+
+public class PianoKeys {
+    // -----------------------------------------------------------------
+    // Prints the number of keys on a piano.
+    // -----------------------------------------------------------------
+    public static void main(String[] args) {
+        int keys = 88;
+        // Because that value is an integer, it is automatically converted to a string
+        // and concatenated with the initial string.
+        System.out.println("A piano has " + keys + " keys");
+
+    }
+}
